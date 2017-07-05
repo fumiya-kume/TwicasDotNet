17f15624913c1a9ebd5fb768bb8aefc4d9612383
Fumiya Kume <fumiya.kume@hotmail.com>
2017-07-05 19:20:31 +0900

    Add GenerateAuthURL Method

---
+++ src/TwicanDotNet/TwicanDotNet/AuthClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TwicanDotNet
+{
+    public class AuthClient
+    {
+        public string ClientID { get; set; } = "";
+        public string CSRF_Token { get; set; } = "";
+        public string GetAuthURL(string ClientID = "", string CSRF_Token = "")
+        {
+            if (string.IsNullOrWhiteSpace(ClientID) || string.IsNullOrWhiteSpace(CSRF_Token)) throw new ArgumentException();
+            return $"https://apiv2.twitcasting.tv/oauth2/authorize?client_id={ClientID}&response_type=code&state={CSRF_Token}";
+        }
+    }
+}
---
+++ src/TwicanDotNet/TwicasDotNet/AuthClientTest.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+using TwicanDotNet;
+using System;
+
+namespace TwicasDotNet
+{
+    public class UnitTest1
+    {
+        private AuthClient client => new AuthClient();
+
+        [Fact]
+        public void AutoURLが正しく発行されるかテスト()
+        {
+            Assert.Throws(typeof(ArgumentException), () => client.GetAuthURL("", ""));
+            Assert.Equal(client.GetAuthURL("Hello", "World"), $"https://apiv2.twitcasting.tv/oauth2/authorize?client_id=Hello&response_type=code&state=World");
+        }
+    }
+}
