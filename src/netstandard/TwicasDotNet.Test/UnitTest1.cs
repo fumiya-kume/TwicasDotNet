07a0fa8c441d2e668b486713eb60655ab2f44147
Fumiya Kume <fumiya.kume@hotmail.com>
2017-07-06 08:30:47 +0900

    Add Analyze Get-AccessKey Request'Response

--- src/netstandard/TwicasDotNet.Test/UnitTest1.cs
+++ src/netstandard/TwicasDotNet.Test/UnitTest1.cs
@@ -12,8 +12,14 @@ namespace TwicasDotNet.Test
         public void AutoURLが正しく発行されるかテスト()
         {
             Assert.Throws(typeof(ArgumentException), () => client.GetAuthURL("", ""));
-            Assert.Equal(client.GetAuthURL("Hello", "World"), $"https://apiv2.twitcasting.tv/oauth2/authorize?client_id=Hello&response_type=code&state=World");
+            Assert.Equal(client.GetAuthURL("Hello", "World"), $"https://apiv2.twitcasting.tv/oauth2/authorize?client_id=Hello&response_type=token&state=World");
         }
-        
+
+        [Fact]
+        public void CallbackURLからアクセストークンを取得できるかテスト()
+        {
+            Assert.Equal("Hello", client.GetAccessTokenFromCallbackURL("http://example.com/#access_token=Hello&token_type=bearer&expires_in=15552000"));
+            //Assert.Equal("", client.GetAccessTokenFromCallbackURL("http://example.com/#result=denied"));
+        }
     }
 }
--- src/netstandard/TwicasDotNet/AuthClient.cs
+++ src/netstandard/TwicasDotNet/AuthClient.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using TwicasDotNet;
 
 namespace TwicasDotNet
@@ -10,7 +12,16 @@ namespace TwicasDotNet
         public string GetAuthURL(string ClientID = "", string CSRF_Token = "")
         {
             if (string.IsNullOrWhiteSpace(ClientID) || string.IsNullOrWhiteSpace(CSRF_Token)) throw new ArgumentException();
-            return $"https://apiv2.twitcasting.tv/oauth2/authorize?client_id={ClientID}&response_type=code&state={CSRF_Token}";
+            return $"https://apiv2.twitcasting.tv/oauth2/authorize?client_id={ClientID}&response_type=token&state={CSRF_Token}";
+        }
+
+        public string GetAccessTokenFromCallbackURL(string callbackURL)
+        {
+            return callbackURL
+                .Split('#', '&')
+                .Where(s => s.Contains("access_token"))
+                .Select(s => s.Replace("access_token=", ""))
+                .First();
         }
     }
 }
