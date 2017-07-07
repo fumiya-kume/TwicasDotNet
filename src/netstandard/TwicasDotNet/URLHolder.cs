632b5c4354b9d1f99af654410cb8306479494a64
Fumiya Kume <fumiya.kume@hotmail.com>
2017-07-08 08:35:40 +0900

    Add call api url Variant

---
+++ src/netstandard/TwicasDotNet/URLHolder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwicasDotNet
+{
+    public static class URLHolder
+    {
+        public static string APICallBaseURL = "https://apiv2.twitcasting.tv";
+    }
+}
