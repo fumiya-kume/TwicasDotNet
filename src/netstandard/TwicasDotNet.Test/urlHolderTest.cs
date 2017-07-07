f29486af76b0c591f5c66e958bcdc95448f32439
Fumiya Kume <fumiya.kume@hotmail.com>
2017-07-08 08:39:47 +0900

    Test for Add Call api url Vatiant

---
+++ src/netstandard/TwicasDotNet.Test/urlHolderTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+using TwicasDotNet;
+
+namespace TwicasDotNet.Test
+{
+    public class urlHolderTest
+    {
+        [Fact]
+        public void APIを呼ぶためのURLが取得できるか()
+        {
+            Assert.Equal("https://apiv2.twitcasting.tv", URLHolder.APICallBaseURL);
+        }
+    }
+}
